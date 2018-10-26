ba5d2975760fa2bf1e4aa6e5758a5f451ceea393
Victoria Martinez <vmartinez72@live.com>
2018-10-26 16:40:51 -0700

    add function if user submits without entry

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,35 @@
 function resultBeepBoop(inputNo) {
+  if (inputNo = " ") {
+    var noInput = ("You have to entry a number!");
+    return noInput;
+  }
   var resultDivByThree = [];
   for (var i = 0; i <= inputNo; i ++) {
     if (i % 3 === 0) {
-     resultDivByThree.push("I'm sorry, Dave. I'm afraid I can't do that. ");
-   } else if (i.toString().includes("0") || i === 0) {
-     resultDivByThree.push("Beep! ");
-   } else if (i.toString().includes("1") || i === 1) {
-     resultDivByThree.push("Boop! ");
+      resultDivByThree.push("I'm sorry, Dave. I'm afraid I can't do that. ");
+    } else if (i.toString().includes("0") || i === 0) {
+      resultDivByThree.push("Beep! ");
+    } else if (i.toString().includes("1") || i === 1) {
+      resultDivByThree.push("Boop! ");
+    }
+    return resultDivByThree;
   }
-  return resultDivByThree;
-}
 
 }
 
 
 
 
-  $(document).ready(function(){
-    $("#inputBeepBoop").submit(function(event){
-      event.preventDefault();
+$(document).ready(function(){
+  $("#inputBeepBoop").submit(function(event){
+    event.preventDefault();
 
-      var inputNumber = parseInt($("#inputNumber").val());
-      var result = resultBeepBoop(inputNumber);
-  console.log(result);
-      // $(".resultBB").empty();
-      $(".resultBB").text(result);
+    var inputNumber = parseInt($("#inputNumber").val());
+    var result = resultBeepBoop(inputNumber);
+    console.log(result);
+    // $(".resultBB").empty();
+    $(".resultBB").text(result);
 
 
-    });
   });
+});
