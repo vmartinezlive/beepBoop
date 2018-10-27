ffe14398e814d24319e2b9e04385c794417daac3
Victoria Martinez <vmartinez72@live.com>
2018-10-26 17:03:14 -0700

    change function on js (code is broken)

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,20 @@ function resultBeepBoop(inputNo) {
   if (inputNo = " ") {
     var noInput = ("You have to entry a number!");
     return noInput;
-  }
-  var resultDivByThree = [];
-  for (var i = 0; i <= inputNo; i ++) {
-    if (i % 3 === 0) {
-      resultDivByThree.push("I'm sorry, Dave. I'm afraid I can't do that. ");
-    } else if (i.toString().includes("0") || i === 0) {
-      resultDivByThree.push("Beep! ");
-    } else if (i.toString().includes("1") || i === 1) {
-      resultDivByThree.push("Boop! ");
+  } else if (i % 3 === 0) {
+    var resultDivByThree = [];
+    for (var i = 0; i < inputNo; i ++) {
+      if (i % 3 === 0) {
+        resultDivByThree.push("I'm sorry, Dave. I'm afraid I can't do that. ");
+      } else if (i.toString().includes("0") || i === 0) {
+        resultDivByThree.push("Beep! ");
+      } else if (i.toString().includes("1") || i === 1) {
+        resultDivByThree.push("Boop! ");
+      }
+      return resultDivByThree;
     }
-    return resultDivByThree;
   }
-
 }
-
 
 
 
@@ -26,7 +25,7 @@ $(document).ready(function(){
 
     var inputNumber = parseInt($("#inputNumber").val());
     var result = resultBeepBoop(inputNumber);
-    console.log(result);
+
     // $(".resultBB").empty();
     $(".resultBB").text(result);
 
