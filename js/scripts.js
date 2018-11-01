9d955058538d61cd7b8bfdd633c4ebcbdbe20976
Student <example@example.com>
2018-10-31 18:09:27 -0700

    add written description to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,31 @@
-function resultBeepBoop(inputNo) {
-  if (inputNo = " ") {
-    var noInput = ("You have to entry a number!");
-    return noInput;
-  } else if (i % 3 === 0) {
-    var resultDivByThree = [];
-    for (var i = 0; i < inputNo; i ++) {
-      if (i % 3 === 0) {
-        resultDivByThree.push("I'm sorry, Dave. I'm afraid I can't do that. ");
-      } else if (i.toString().includes("0") || i === 0) {
-        resultDivByThree.push("Beep! ");
-      } else if (i.toString().includes("1") || i === 1) {
-        resultDivByThree.push("Boop! ");
-      }
-      return resultDivByThree;
+//Business Logic
+function resultBeepBoop(inputNo) { //function resultBeepBoop is called
+  var resultDivByThree = [];
+
+  for (var i = 0; i <= inputNo; i ++) { // for variable index equals to 0; index is less oe equals to inputNo; index plus plus
+    if (i.toString().includes("0") || i === 0) { //if index to string includes 0 push beep to resultDivByThree.
+      resultDivByThree.push("Beep! ");
+    } else if (i.toString().includes("1") || i === 1) { //else if index to string includes 1 or index is equal to 1, push Boop to resultDivByThree
+      resultDivByThree.push("Boop! ");
+    } else if (i % 3 === 0) { //else if index is modulo (or divisble by 3) equals to 0 push "I'm sorry, Dave. I'm affraid I can't do that" to resultDivByThree
+      resultDivByThree.push("I'm sorry, Dave. I'm affraid I can't do that. ");
+    } else { //else push index to resultDivByThree
+      resultDivByThree.push(i);
     }
   }
+  return resultDivByThree; //resultDivByThree is returned
 }
 
 
-
-$(document).ready(function(){
-  $("#inputBeepBoop").submit(function(event){
+//Business User Interface Logic
+$(document).ready(function(){  //*start HERE:
+  $("#inputBeepBoop").submit(function(event){ // user enters a number and it gets submitted in the document
     event.preventDefault();
 
-    var inputNumber = parseInt($("#inputNumber").val());
-    var result = resultBeepBoop(inputNumber);
+    var inputNumber = parseInt($("#inputNumber").val()); // the inputeNumber is changed to a interger
+    var result = resultBeepBoop(inputNumber); //the inputNumber become a new variable called resultBeepBoop
 
-    // $(".resultBB").empty();
     $(".resultBB").text(result);
-
 
   });
 });
